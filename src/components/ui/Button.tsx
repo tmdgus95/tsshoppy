1c3e2fc63b3a21ae18acbacf23e3c52b906904e8
tmdgus95 <8571053@naver.com>
2023-04-17 22:33:50 +0900

    제품 등록 입력폼 구현

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,6 +1,6 @@
 type Props = {
     text: string;
-    onClick: () => void;
+    onClick?: () => void;
 };
 
 export default function Button({ text, onClick }: Props) {
--- src/pages/NewProduct.tsx
+++ src/pages/NewProduct.tsx
@@ -1,3 +1,79 @@
+import { ChangeEvent, useState } from 'react';
+import Button from '../components/ui/Button';
+
 export default function NewProduct() {
-    return <div>NewProduct</div>;
+    const [product, setProduct] = useState({
+        title: '',
+        price: '',
+        category: '',
+        description: '',
+        options: '',
+    });
+    const [file, setFile] = useState<File | undefined>();
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        const { name, value, files } = e.target;
+        if (name === 'file' && files && files.length > 0) {
+            setFile(files[0]);
+            return;
+        }
+        setProduct((product) => ({ ...product, [name]: value }));
+    };
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+    };
+    return (
+        <section>
+            {file && <img src={URL.createObjectURL(file)} alt='local file' />}
+            <form onSubmit={handleSubmit}>
+                <input
+                    type='file'
+                    accept='image/*'
+                    name='file'
+                    required
+                    onChange={handleChange}
+                />
+                <input
+                    type='text'
+                    name='title'
+                    value={product.title ?? ''}
+                    placeholder='제품명'
+                    required
+                    onChange={handleChange}
+                />
+                <input
+                    type='number'
+                    name='price'
+                    value={product.price ?? ''}
+                    placeholder='가격'
+                    required
+                    onChange={handleChange}
+                />
+                <input
+                    type='number'
+                    name='category'
+                    value={product.category ?? ''}
+                    placeholder='카테고리'
+                    required
+                    onChange={handleChange}
+                />
+                <input
+                    type='number'
+                    name='description'
+                    value={product.description ?? ''}
+                    placeholder='제품 설명'
+                    required
+                    onChange={handleChange}
+                />
+                <input
+                    type='number'
+                    name='options'
+                    value={product.options ?? ''}
+                    placeholder='옵션들(콤마(,)로 구분)'
+                    required
+                    onChange={handleChange}
+                />
+                <Button text='제품 등록하기' />
+            </form>
+        </section>
+    );
 }
