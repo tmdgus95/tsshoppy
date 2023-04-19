ac4a8896399df217d0dcee9865211e979e7ac6bc
tmdgus95 <8571053@naver.com>
2023-04-19 22:15:56 +0900

    장바구니 UI 디자인

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -5,10 +5,14 @@ import {
 } from '../api/firebase';
 import { AiOutlineMinusSquare, AiOutlinePlusSquare } from 'react-icons/ai';
 import { RiDeleteBin5Fill } from 'react-icons/ri';
+
 type Props = {
     product: CartProduct;
     uid: string;
 };
+
+const ICON_CLASS =
+    'transition-all cursor-pointer hover:text-brand hover:scale-105 mx-1';
 export default function CartItem({
     product,
     product: { id, image, title, option, quantitiy, price },
@@ -22,16 +26,28 @@ export default function CartItem({
         addOrUpdatedToCart(uid, { ...product, quantitiy: quantitiy + 1 });
     const handleDelete = () => removeFromCart(uid, id);
     return (
-        <li>
-            <img src={image} alt={title} />
-            <div>
-                <p>{title}</p>
-                <p>{option}</p>
-                <div>
-                    <AiOutlineMinusSquare onClick={handleMinus} />
+        <li className='flex justify-between my-2 items-center'>
+            <img className='w-24 md:w-48 rounded-lg' src={image} alt={title} />
+            <div className='flex-1 flex justify-between ml-4'>
+                <div className='basis-3/5'>
+                    <p className='text-lg'>{title}</p>
+                    <p className='text-lg font-bold text-brand'>{option}</p>
+                    <p>￦{price}</p>
+                </div>
+                <div className='text-2xl flex items-center'>
+                    <AiOutlineMinusSquare
+                        className={ICON_CLASS}
+                        onClick={handleMinus}
+                    />
                     <span>{quantitiy}</span>
-                    <AiOutlinePlusSquare onClick={handlePlus} />
-                    <RiDeleteBin5Fill onClick={handleDelete} />
+                    <AiOutlinePlusSquare
+                        className={ICON_CLASS}
+                        onClick={handlePlus}
+                    />
+                    <RiDeleteBin5Fill
+                        className={ICON_CLASS}
+                        onClick={handleDelete}
+                    />
                 </div>
             </div>
         </li>
--- src/components/PriceCard.tsx
+++ src/components/PriceCard.tsx
@@ -4,5 +4,12 @@ type Props = {
 };
 
 export default function PriceCard({ text, price }: Props) {
-    return <div>{text}</div>;
+    return (
+        <div className='bg-gray-50 p-8 mx-2 rounded-2xl text-center text-lg md:text-xl'>
+            <p>{text}</p>
+            <p className='font-bold text-brand text-xl md:text-2xl'>
+                ￦{price}
+            </p>
+        </div>
+    );
 }
